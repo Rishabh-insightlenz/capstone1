7a89a28edabdfd71e4f64fa3db9303ae7cadff5d
whygithub-python <61707398+whygithub-python@users.noreply.github.com>
2020-03-03 00:26:45 +0530

    Add files via upload

---
+++ Capstone project 1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Mainly Used for Capstone project
+
+# In[2]:
+
+
+import numpy as np
+import pandas as pd
+
+
+# In[3]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
